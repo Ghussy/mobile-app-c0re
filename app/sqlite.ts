02f510946de9e0f09baa667f5d35536425790f2c
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-08 23:30:12 -0700

    Add location history sqlite functions

--- app/sqlite.ts
+++
@@ -1,8 +0,0 @@
-import SQLite from "react-native-sqlite-storage";
-
-const dbPromise = SQLite.openDatabase({ name: "db.sqlite" });
-
-export async function sql(query: string): Promise<SQLite.ResultSet[]> {
-	const db = await dbPromise;
-	return db.executeSql(query);
-}
