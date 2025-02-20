12e4678269a07545de52e41681cdf7a0785a76ed
Gunnar Huscroft <44684537+Ghussy@users.noreply.github.com>
2025-02-19 21:05:50 -0700

    Update Prettier save setting

--- app/(setup)/_layout.tsx
+++ app/(setup)/_layout.tsx
@@ -7,4 +7,4 @@ export default function SetupLayout() {
       <Stack.Screen name="setGym" />
     </Stack>
   );
-} 
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,14 +5,14 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import { LocationReporter } from "@/providers/LocationReporter";
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import {
   useFonts,
   Inter_400Regular,
   Inter_500Medium,
   Inter_600SemiBold,
   Inter_700Bold,
-} from '@expo-google-fonts/inter';
+} from "@expo-google-fonts/inter";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -21,7 +21,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default gestureHandlerRootHOC(function RootLayout() {
   const [loaded] = useFonts({
-    'Orbitron-Bold': require('../assets/fonts/Orbitron-Bold.ttf'),
+    "Orbitron-Bold": require("../assets/fonts/Orbitron-Bold.ttf"),
     Inter_400Regular,
     Inter_500Medium,
     Inter_600SemiBold,
@@ -49,4 +49,3 @@ export default gestureHandlerRootHOC(function RootLayout() {
     </ThemeProvider>
   );
 });
-
