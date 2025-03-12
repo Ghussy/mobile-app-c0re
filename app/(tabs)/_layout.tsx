512fd978e550ecdcfdf5aecb56c7a4377a356ea4
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-03-12 16:43:58 -0600

    Fix auth screen flashing

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,14 @@
-import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
-import { Redirect } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { useAuth } from "@/lib/supabase";
 import { enrolledGyms } from "@/lib/sqlite";
 import { useEffect, useState } from "react";
 
 export default function TabsLayout() {
   const auth = useAuth();
-
-  if (!auth.user) {
-    console.log("Not signed in, redirecting to auth");
-    return <Redirect href={"/(auth)"} />;
-  }
-
-  if (typeof auth.gymGoal === "undefined") {
-    console.log("No gym goal, redirecting");
-    return <Redirect href={"/(setup)/setGoal"} />;
-  }
-
   const [shouldRedirect, setRedirect] = useState(false);
+
   useEffect(() => {
     enrolledGyms().then((gyms) => {
       if (gyms.length === 0) {
@@ -28,8 +17,19 @@ export default function TabsLayout() {
     });
   }, []);
 
+  if (!auth.isInitialized) {
+    return null;
+  }
+
+  if (!auth.user) {
+    return <Redirect href="/(auth)" />;
+  }
+
+  if (typeof auth.gymGoal === "undefined") {
+    return <Redirect href="/(setup)/setGoal" />;
+  }
+
   if (shouldRedirect) {
-    console.log("User has not enrolled in any gyms- redirecting");
     return <Redirect href="/(setup)/setGym" />;
   }
 
